5ad924651132f33829c05871d5ae3afd75a981e6
Emma Cushman <acushman@gmail.com>
2016-05-24 19:49:42 -0700

    add heroku-specific configuration for deployment

---
+++ nuremberg/settings/staging.py
@@ -0,0 +1,16 @@
+from .generic import *
+import dj_database_url
+
+DEBUG = False
+ALLOWED_HOSTS = [".herokuapp.com"]
+
+COMPRESS_ENABLED = True
+
+# such that the precompiler will only auto-compile on manage.py compress
+STATIC_PRECOMPILER_DISABLE_AUTO_COMPILE = False
+COMPRESS_OFFLINE = True
+
+DATABASES['default'] = dj_database_url.config(env='CLEARDB_DATABASE_URL')
+del DATABASES['default']['OPTIONS']['reconnect'] # not supported by mysqlclient apparently
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
