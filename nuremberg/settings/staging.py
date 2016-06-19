745c29df5ed712f189b03c14e0df59758a679d47
Emma Cushman <acushman@gmail.com>
2016-06-18 18:20:05 -0700

    websolr config for heroku

--- nuremberg/settings/staging.py
+++ nuremberg/settings/staging.py
@@ -13,4 +13,11 @@ COMPRESS_OFFLINE = True
 DATABASES['default'] = dj_database_url.config(env='CLEARDB_DATABASE_URL')
 del DATABASES['default']['OPTIONS']['reconnect'] # not supported by mysqlclient apparently
 
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'nuremberg.search.lib.solr_grouping_backend.GroupedSolrEngine',
+        'URL': os.environ.get('WEBSOLR_URL', ''),
+    },
+}
+
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
