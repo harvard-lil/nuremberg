64284bc22a3fdd99f8c6db4858eddcf54d83b244
Ben Steinberg <bsteinberg@law.harvard.edu>
2016-12-05 16:33:31 -0500

    Ignore empty user-agent

--- nuremberg/core/middlewares/crawler.py
+++ nuremberg/core/middlewares/crawler.py
@@ -5,13 +5,12 @@ bots = ["Baiduspider"]
 class BlockCrawlerMiddleware:
     def process_request(self, request):
         user_agent = request.META.get('HTTP_USER_AGENT', None)
-        if not user_agent:
-            return HttpResponseForbidden('Requests without user-agent are not supported.')
         request.is_crawler = False
 
-        for bot in bots:
-            if bot in user_agent:
-                request.is_crawler = True
+        if user_agent:
+            for bot in bots:
+                if bot in user_agent:
+                    request.is_crawler = True
 
         if request.is_crawler and request.path.startswith("/search/"):
             return HttpResponseForbidden('This address is removed from crawling. Check robots.txt')
